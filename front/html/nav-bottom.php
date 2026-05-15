0fd43bf773c1b4e46bd41e5c3abb98b0483bcbcc
Agden <chouw59@hotmail.fr>
2026-05-15 22:09:54 +0200

    separation en deux fichiers de nav-haut et nav-bas

---
+++ front/html/nav-bottom.php
@@ -0,0 +1,5 @@
+<!-- Barre de navigation BAS mobile/Tablette -->
+<nav class="nav-b-mob">
+    <a href="../html/search-trajet.php" class="nav-b-btn"><i class="bi bi-search"></i></a>
+    <a href="../html/propo-trajet.php" class="nav-b-btn"><i class="fas fa-road"></i></a>
+</nav>
--- front/html/nav-top.php
+++ front/html/nav-top.php
@@ -66,9 +66,3 @@
         </li>
     </ul>
 </nav>
-
-<!-- Barre de navigation BAS mobile/Tablette -->
-<nav class="nav-b-mob">
-    <a href="../html/search-trajet.php" class="nav-b-btn"><i class="bi bi-search"></i></a>
-    <a href="../html/propo-trajet.php" class="nav-b-btn"><i class="fas fa-road"></i></a>
-</nav>
