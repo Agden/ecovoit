db78fa280f882e0bf9c3f26594ec7319828d105a
Agden <chouw59@hotmail.fr>
2026-05-15 22:06:23 +0200

    créa du fichier de factorisation du navigateur

---
+++ front/html/nav.php
@@ -0,0 +1,74 @@
+<?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
+
+<!-- Barre de navigation mobile/tablette (haut) -->
+<nav class="nav-h-mob">    
+    <button class="nav-h-btn-profil"><i class="bi bi-plus-circle-fill"></i></button>
+    <div class="profil-mob-deroulant" id="profil-menu">
+        <ul class="deroulage-services">
+            <li><a href="../html/profil-user.php">Mon profil</a></li>
+            <li><a href="../html/propo-trajet.php">Proposer un trajet</a></li>
+            <li><a href="../html/propo-trajet.php#mes-trajets-en-cours">Trajets proposés</a></li>
+            <li><a href="../html/hist-conducteur.php">Historique conducteur</a></li>
+            <li><a href="../html/hist-passager.php">Historique passager</a></li>
+            <li><a href="../html/parametre.php">Paramètres</a></li>
+        </ul>
+    </div>
+    <a href="../html/profil-user.php" class="nav-h-btn">
+        <i class="bi bi-person-fill"></i>
+    </a>
+</nav>
+
+<!-- Barre de navigation Web -->
+<nav class="container-nav">
+    <div class="logo-text">
+        <div class="logo">
+            <a href="../html/accueil.php">
+                <img src="../img/arbre.jpg" alt="Logo">
+            </a>
+        </div>
+        <p class="text-titre">EcoVoit'</p>
+    </div>
+    <ul class="lien-nav">
+        <li class="trajet">
+            <a href="../html/search-trajet.php">
+                Trouver un trajet
+                <div class="souligne"></div>
+            </a>
+        </li>
+        <li class="contact">
+            <a href="../html/contact.php">
+                Contact
+                <div class="souligne"></div>
+            </a>
+        </li>
+        <li class="accueil">
+            <a href="#">
+                Autres services
+                <div class="souligne"></div>
+            </a>
+            <div class="deroulant-services">
+                <ul class="deroulage-services">
+                    <li><a href="../html/profil-user.php">Mon profil</a></li>
+                    <li><a href="../html/propo-trajet.php">Proposer un trajet</a></li>
+                    <li><a href="../html/propo-trajet.php#mes-trajets-en-cours">Trajets proposés</a></li>
+                    <li><a href="../html/hist-conducteur.php">Historique conducteur</a></li>
+                    <li><a href="../html/hist-passager.php">Historique passager</a></li>
+                    <li><a href="../html/parametre.php">Paramètres</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="connexion">
+            <?php if(isset($_SESSION["user_id"])): ?>
+                <a href="profil-user.php">Mon profil</a>
+            <?php else: ?>
+                <a href="/index.php">Se connecter</a>
+            <?php endif; ?>
+        </li>
+    </ul>
+</nav>
+
+<!-- Barre de navigation BAS mobile/Tablette -->
+<nav class="nav-b-mob">
+    <a href="../html/search-trajet.php" class="nav-b-btn"><i class="bi bi-search"></i></a>
+    <a href="../html/propo-trajet.php" class="nav-b-btn"><i class="fas fa-road"></i></a>
+</nav>
